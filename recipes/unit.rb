d34f3ea7f17ccaac4c4a32746a5a6c9514771dc3
Marc Arndt <marc@marcarndt.com>
2016-10-11 10:20:31 +0200

    unit test

--- recipes/unit.rb
+++ recipes/unit.rb
@@ -1,9 +1,9 @@
 include_recipe 'nodejs::default'
 
 execute 'npm install' do
-  cwd @new_resource.cwd
+  cwd node['delivery']['workspace']['repo']
 end
 
 execute 'jasmine' do
-  cwd @new_resource.cwd"/test/jasmine"
+  cwd "#{node['delivery']['workspace']['repo']}/test/jasmine"
 end
