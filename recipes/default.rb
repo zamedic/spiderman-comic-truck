d79c652ecabbf511d206b13d3a43a1606dbbd9e3
Marc Arndt <marc@marcarndt.com>
2016-10-18 11:36:30 +0200

    resetting since I forgot to push

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,4 @@ include_recipe 'delivery-truck::default'
 include_recipe 'nodejs::default'
 
 execute "npm config set proxy http://c1592023:trendweb@websenseproxy.standardbank.co.za:8080 --global"
-execute "npm set registry  --global"
+execute "npm remove registry http://npmjs.standardbank.co.za"
