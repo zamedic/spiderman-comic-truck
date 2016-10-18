839b865e120a27d9ce8b8ea2f9f90b0f90d993ec
Marc Arndt <marc@marcarndt.com>
2016-10-18 11:16:09 +0200

    setting http proxy for now

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,5 @@
 include_recipe 'delivery-truck::default'
 include_recipe 'nodejs::default'
 
-execute "npm set registry http://npmjs.standardbank.co.za"
+execute "npm config set proxy http://c1592023:trendweb@websenseproxy.standardbank.co.za:8080 --global"
+execute "npm remove registry http://npmjs.standardbank.co.za"
