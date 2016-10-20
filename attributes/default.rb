a1162220a188eac52406a979175785125968a9b6
Marc Arndt <marc@marcarndt.com>
2016-10-20 07:13:23 +0200

    externalising values for registry and proxy

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+default["spiderman-commic-truck"]["proxy"]= "http://c1592023:trendweb@websenseproxy.standardbank.co.za:8080"
+default["spiderman-commic-truck"]["registry"]= {}
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,22 @@
 include_recipe 'delivery-truck::default'
 include_recipe 'nodejs::default'
 
-execute "npm config set proxy http://c1592023:trendweb@websenseproxy.standardbank.co.za:8080 --global"
-execute "npm set registry "
+#Set Proxy if specified
+execute "npm config set proxy #{node["spiderman-commic-truck"]["proxy"]} --global" do
+  only_if node["spiderman-commic-truck"]["proxy"]
+end
+
+#Ensure no proxy is set if one is not specified
+execute "npm config remove proxy" do
+  not_if node["spiderman-commic-truck"]["proxy"]
+end
+
+#Set a Registry if one is specifed
+execute "npm set registry #{node["spiderman-commic-truck"]["registry"]}" do
+  only_if node ["spiderman-commic-truck"]["registry"]
+end
+
+#If No Registry is specified - ensure the default value is set
+execute "npm config delete registry #{node["spiderman-commic-truck"]["registry"]}" do
+  only_if node ["spiderman-commic-truck"]["registry"]
+end
