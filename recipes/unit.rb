4e47ae7e4c8083586e9503e7c4bf5d4d01041161
Marc Arndt <marc@marcarndt.com>
2016-10-21 09:25:33 +0200

    setting default to nil and doing nil checks

--- recipes/unit.rb
+++ recipes/unit.rb
@@ -5,5 +5,5 @@ end
 
 execute 'jasmine' do
   cwd "#{node['delivery']['workspace']['repo']}/#{node["spiderman-commic-truck"]["unit"]["jasmine"]}"
-  only_if node["spiderman-commic-truck"]["unit"]["jasmine"] != nil
+  only_if {node["spiderman-commic-truck"]["unit"]["jasmine"] != nil}
 end
