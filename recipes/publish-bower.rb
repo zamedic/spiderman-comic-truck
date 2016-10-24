48b9a10cce824755c978f8229c60e89d2bc86591
Marc Arndt <marc@marcarndt.com>
2016-10-24 13:37:36 +0200

    adding bower build

--- recipes/publish-bower.rb
+++ recipes/publish-bower.rb
@@ -1,3 +1,3 @@
-execute "#{node['delivery']['workspace']['repo']}/node_modules/.bin/bower" do
+execute "#{node['delivery']['workspace']['repo']}/node_modules/.bin/bower install" do
   cwd "#{node['delivery']['workspace']['repo']}/"
 end
