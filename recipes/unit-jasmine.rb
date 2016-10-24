0429d269f403a6351c1725dc0728fa76c31719c1
Marc Arndt <marc@marcarndt.com>
2016-10-24 10:37:34 +0200

    adding jasmine

--- recipes/unit-jasmine.rb
+++ recipes/unit-jasmine.rb
@@ -1,5 +1,5 @@
 nodejs_npm 'jasmine'
 
 execute 'jasmine' do
-  cwd "#{node['delivery']['workspace']['repo']}/#{node["spiderman-commic-truck"]["unit"]["jasmine"]["path"]}"
+  cwd "#{node['delivery']['workspace']['repo']}/#{node["spiderman-comic-truck"]["unit"]["jasmine"]["path"]}"
 end
