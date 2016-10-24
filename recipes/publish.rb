d12607b00bc638f5e6096c0df3e67da44b6ac7ec
Marc Arndt <marc@marcarndt.com>
2016-10-24 13:27:03 +0200

    adding publish steps

--- recipes/publish.rb
+++ recipes/publish.rb
@@ -1,3 +1,7 @@
+execute 'npm install' do
+  cwd node['delivery']['workspace']['repo']
+end
+
 include_recipe "spiderman-comic-truck::publish-bower"
 include_recipe "spiderman-comic-truck::publish-grunt"
 
