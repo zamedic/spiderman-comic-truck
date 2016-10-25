208c5355f096455a49fb48233622d8fd2fffdd76
Marc Arndt <marc@marcarndt.com>
2016-10-25 07:49:20 +0200

    adding syntax check

--- recipes/syntax.rb
+++ recipes/syntax.rb
@@ -1,7 +1,11 @@
 include_recipe 'delivery-truck::syntax'
 
+unless ::File.exist?("#{node['delivery']['workspace']['repo']}/package.json")
+  raise RuntimeError, "This delivery is meant to build npm based applications. I cannot find a package.json so I need to abort the build"
+end
+
 unless bumped_npm_version?(node['delivery']['workspace']['repo'])
-  raise RuntimeError, "Artifact version unchanged - you have to increase the version in the package.json file"
+  raise RuntimeError, "Artifact version unchanged from parent - you have to increase the version in the package.json file"
 end
 
 
