8a3ef9508d19fc4229775054310c0df9518c1769
Marc Arndt <marc@marcarndt.com>
2016-10-25 07:18:32 +0200

    adding grunt build

--- recipes/publish-grunt.rb
+++ recipes/publish-grunt.rb
@@ -1,3 +1,3 @@
-execute "#{node['delivery']['workspace']['repo']}/node_modules/.bin/grunt install" do
+execute "#{node['delivery']['workspace']['repo']}/node_modules/.bin/grunt build" do
   cwd "#{node['delivery']['workspace']['repo']}/"
 end
