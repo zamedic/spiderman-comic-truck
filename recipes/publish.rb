ecc96259c3a8fec170e1c2e07d174df5adf7c0a3
Marc Arndt <marc@marcarndt.com>
2016-11-03 12:33:38 +0200

    fixing publish since we cannot install global modules

--- recipes/publish.rb
+++ recipes/publish.rb
@@ -8,7 +8,7 @@ include_recipe "spiderman-comic-truck::publish-bower" if bower
 grunt = ::File.exist?("#{node['delivery']['workspace']['repo']}/Gruntfile.js")
 include_recipe "spiderman-comic-truck::publish-grunt" if grunt
 
-execute 'npm install -g npm-cli-login'
+execute 'npm install npm-cli-login'
 
 if (node["spiderman-comic-truck"]["deploy_registry"] == nil)
   registry = node["spiderman-comic-truck"]["registry"]
@@ -16,9 +16,13 @@ else
   registry = node["spiderman-comic-truck"]["deploy_registry"]
 end
 
-execute "npm-cli-login -u #{node["spiderman-comic-truck"]["deploy"]["user"]} -p #{node["spiderman-comic-truck"]["deploy"]["password"]} -e #{node["spiderman-comic-truck"]["deploy"]["email"]} -r #{registry} --scope @#{node["spiderman-comic-truck"]["deploy"]["scope"]}"
+execute "./node-modules/.bin/npm-cli-login -u #{node["spiderman-comic-truck"]["deploy"]["user"]} -p #{node["spiderman-comic-truck"]["deploy"]["password"]} -e #{node["spiderman-comic-truck"]["deploy"]["email"]} -r #{registry} --scope @#{node["spiderman-comic-truck"]["deploy"]["scope"]}" do
+  cwd node['delivery']['workspace']['repo']
+end
 
-execute "npm publish -r #{registry}"
+execute "npm publish -r #{registry}" do
+  cwd node['delivery']['workspace']['repo']
+end
 
 include_recipe 'delivery-truck::publish'
 
