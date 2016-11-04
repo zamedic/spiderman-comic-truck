507c044eb17977fff72807c6752fad9af818e3eb
Marc Arndt <marc@marcarndt.com>
2016-11-04 07:43:59 +0200

    adding expect scripts to try and log the user in

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,10 @@ default["spiderman-comic-truck"]["proxy"]= nil
 default["spiderman-comic-truck"]["registry"]= nil
 
 default["spiderman-comic-truck"]["deploy"]["registry"]= nil
-default["spiderman-comic-truck"]["deploy"]["auth-token"]= nil
+default["spiderman-comic-truck"]["deploy"]["user"]= nil
+default["spiderman-comic-truck"]["deploy"]["password"]= nil
+default["spiderman-comic-truck"]["deploy"]["email"]= nil
+default["spiderman-comic-truck"]["deploy"]["scope"]= nil
 
 
 
--- recipes/publish.rb
+++ recipes/publish.rb
@@ -9,23 +9,38 @@ grunt = ::File.exist?("#{node['delivery']['workspace']['repo']}/Gruntfile.js")
 include_recipe "spiderman-comic-truck::publish-grunt" if grunt
 
 
-
 if (node["spiderman-comic-truck"]["deploy"]["registry"] == nil)
   registry = node["spiderman-comic-truck"]["registry"]
 else
   registry = node["spiderman-comic-truck"]["deploy"]["registry"]
 end
 
-ruby_block "insert_line" do
-  block do
-    file = Chef::Util::FileEdit.new("~/.npmrc")
-    file.insert_line_if_no_match("//#{registry}:_authToken=#{node["spiderman-comic-truck"]["deploy"]["auth-token"]}")
-    file.write_file
-  end
-end
 execute "npm set registry #{registry}" do
   cwd node['delivery']['workspace']['repo']
 end
+
+bash 'setup username and password' do
+  cwd ::File.dirname('/usr/local/bin')
+  code <<-EOH
+    #!/usr/bin/expect
+
+    spawn "npm login"
+
+    expect "Username:"
+
+    send "#{node["spiderman-comic-truck"]["deploy"]["user"]}\r"
+
+    expect "Password:"
+
+    send "#{node["spiderman-comic-truck"]["deploy"]["password"]}\r"
+
+    expect "Email: (this IS public)"
+
+    send "#{node["spiderman-comic-truck"]["deploy"]["email"]}\r"
+
+  EOH
+end
+
 
 execute "npm publish" do
   cwd node['delivery']['workspace']['repo']
