6745fc51dbd35917838e9f8f17ce0b51c7f42606
Marc Arndt <marc@marcarndt.com>
2016-11-10 14:08:43 +0200

    adding grunt logging

--- recipes/publish-grunt.rb
+++ recipes/publish-grunt.rb
@@ -1,3 +1,3 @@
-execute "#{node['delivery']['workspace']['repo']}/node_modules/.bin/grunt build" do
+execute "#{node['delivery']['workspace']['repo']}/node_modules/.bin/grunt build > #{node['delivery']['workspace']['repo']}/grunt.log" do
   cwd "#{node['delivery']['workspace']['repo']}/"
 end
